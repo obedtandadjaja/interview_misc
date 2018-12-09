53ca7e5d4a9f5570d5e0a113551e7c55a329f23a
Obed Tandadjaja <obed.tandadjaja@gmail.com>
2018-12-09 01:16:36 -0800

    add DeckPartition

---
+++ interview201811/general/DeckPartition.java
@@ -0,0 +1,67 @@
+// In a deck of cards, each card has an integer written on it.
+// Return true if and only if you can choose X >= 2 such that it is possible to split the
+// entire deck into 1 or more groups of cards, where:
+//     1. Each group has exactly X cards.
+//     2. All the cards in each group have the same integer.
+
+//  Example 1:
+
+//  Input: [1,2,3,4,4,3,2,1]
+//  Output: true
+//  Explanation: Possible partition [1,1],[2,2],[3,3],[4,4]
+
+//  Example 2:
+//  Input: [1,1,1,2,2,2,3,3]
+//  Output: false
+//  Explanation: No possible partition.
+
+//  Example 3:
+//  Input: [1]
+//  Output: false
+//  Explanation: No possible partition.
+
+//  Example 4:
+//  Input: [1,1]
+//  Output: true
+//  Explanation: Possible partition [1,1]
+
+//  Example 5:
+//  Input: [1,1,2,2,2,2]
+//  Output: true
+//  Explanation: Possible partition [1,1],[2,2],[2,2]
+
+//  Note:
+//  1 <= deck.length <= 10000
+//  0 <= deck[i] < 10000
+
+import java.util.*;
+
+class PartitionDeck {
+    public static void main(String[] args) {
+        int[] deck = new int[] { 1, 1, 2, 2, 3, 3 };
+        hasGroupsSizeX(deck);
+    }
+
+    public static boolean hasGroupsSizeX(int[] deck) {
+        int[] counts = new int[10000];
+        for(int i: deck) counts[i]++;
+
+        int partition_length = 0;
+        for(int count: counts) {
+            if(count == 0) continue;
+
+            if(partition_length == 0) {
+                partition_length = count;
+            } else {
+                int gcd = gcd(count, partition_length);
+                if(gcd <= 1) return false;
+            }
+        }
+
+        return partition_length >= 2;
+    }
+
+    public static int gcd(int x, int y) {
+        return x == 0 ? y : gcd(y%x, x);
+    }
+}
