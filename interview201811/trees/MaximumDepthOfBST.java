0332895c69c9369553dfbcf4918f16ec6d4678eb
Obed Tandadjaja <obed.tandadjaja@gmail.com>
2018-12-15 23:49:03 -0800

    add MaximumDepthOfBST

---
+++ interview201811/trees/MaximumDepthOfBST.java
@@ -0,0 +1,41 @@
+/*
+  Difficulty: Easy
+
+  Given a binary tree, find its maximum depth.
+
+  The maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
+
+  Note: A leaf is a node with no children.
+
+  Example:
+
+  Given binary tree [3,9,20,null,null,15,7],
+
+  3
+  / \
+  9  20
+  /  \
+  15   7
+  return its depth = 3.
+ */
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class MaximumDepthOfBST {
+  public int maxDepth(TreeNode root) {
+    return getDepth(root, 0);
+  }
+
+  public int getDepth(TreeNode root, int depth) {
+    if(root == null) return 0;
+
+    return 1 + Math.max(getDepth(root.left, depth), getDepth(root.right, depth));
+  }
+}
