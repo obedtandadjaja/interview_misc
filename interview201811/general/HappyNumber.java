fddb8ec40e66ad892c7b2b1ceff75736e8045337
Obed Tandadjaja <obed.tandadjaja@gmail.com>
2018-12-16 00:58:51 -0800

    add HappyNumber

---
+++ interview201811/general/HappyNumber.java
@@ -0,0 +1,42 @@
+/*
+  Difficulty: Easy
+
+  Write an algorithm to determine if a number is "happy".
+
+  A happy number is a number defined by the following process: Starting with any positive integer, replace the number by the sum of the squares of its digits, and repeat the process until the number equals 1 (where it will stay), or it loops endlessly in a cycle which does not include 1. Those numbers for which this process ends in 1 are happy numbers.
+
+  Example:
+
+  Input: 19
+  Output: true
+  Explanation:
+  1^2 + 9^2 = 82
+  8^2 + ^2^2 = 68
+  6^2 + 8^2 = 100
+  1^2 + 0^2 + 0^2 = 1
+ */
+
+class HappyNumber {
+  public boolean isHappy(int n) {
+    return isHappyRecursive(n, new HashSet<Integer>());
+  }
+
+  public boolean isHappyRecursive(int n, HashSet<Integer> set) {
+    int sum = 0;
+
+    while(n > 0) {
+      int x = n % 10;
+      sum += x * x;
+      n /= 10;
+    }
+
+    if(sum == 1)
+      return true;
+    else if(set.contains(sum))
+      return false;
+    else
+      set.add(sum);
+
+    return isHappyRecursive(sum, set);
+  }
+}
