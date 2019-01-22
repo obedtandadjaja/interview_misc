8f8a1af30f74dc7e025026be23a3b074a1437e03
Obed Tandadjaja <obed.tandadjaja@gmail.com>
2019-01-22 14:23:58 -0800

    update MaximalSquare

--- interview201811/dp/MaximalSquare.java
+++ interview201811/dp/MaximalSquare.java
@@ -1,8 +1,46 @@
+/*
+  Difficulty: Medium
+
+  Given a 2D binary matrix filled with 0's and 1's, find the largest square containing only 1's and return its area.
+
+  Example:
+
+  Input: 
+
+  1 0 1 0 0
+  1 0 1 1 1
+  1 1 1 1 1
+  1 0 0 1 0
+
+  Output: 4
+ */
+
 class Solution {
   public int maximalSquare(int[][] matrix) {
     if(matrix.length == 0 || matrix[0].length == 0) return 0;
 
     int max = matrix[0][0];
+
+    // first row
+    if(max == 0) {
+      for(int i = 1; i < matrix[0].length; i++) {
+        if(matrix[0][i] == 1) {
+          max = matrix[0][i];
+          break;
+        }
+      }
+    }
+
+    // first col
+    if(max == 0) {
+      for(int i = 1; i < matrix[0].length; i++) {
+        if(matrix[i][0] == 1) {
+          max = matrix[i][0];
+          break;
+        }
+      }
+    }
+
     for(int i = 1; i < matrix.length; i++) {
       for(int j = 1; j < matrix[0].length; j++) {
         if(matrix[i][j] == 1) {
