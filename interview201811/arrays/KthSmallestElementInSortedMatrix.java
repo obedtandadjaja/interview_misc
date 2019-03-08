b52d06df5d30b093a0a33759cace9cd5ad611fb7
Obed Tandadjaja <obed.tandadjaja@gmail.com>
2019-03-08 00:09:59 -0800

    add KthSmallestElementInSortedMatrix

---
+++ interview201811/arrays/KthSmallestElementInSortedMatrix.java
@@ -0,0 +1,34 @@
+/*
+  Difficulty: Medium
+
+  Given a n x n matrix where each of the rows and columns are sorted in ascending order, find the kth smallest element in the matrix.
+
+  Note that it is the kth smallest element in the sorted order, not the kth distinct element.
+
+  Example:
+
+  matrix = [
+  [ 1,  5,  9],
+  [10, 11, 13],
+  [12, 13, 15]
+  ],
+  k = 8,
+
+  return 13.
+ */
+
+class Solution {
+  public int kthSmallest(int[][] matrix, int k) {
+    PriorityQueue<Integer> q = new PriorityQueue<Integer>(Collections.reverseOrder());
+
+    for(int i = 0; i < matrix.length; i++) {
+      for(int j = 0; j < matrix.length; j++) {
+        q.add(matrix[i][j]);
+      }
+    }
+
+    while(q.size() > k) q.poll();
+
+    return q.poll();
+  }
+}
